c6c63274e31d92ceba9582f731c6f4c3ff6d7426
Victor-prog-lab <selzlein.chaves@escola.pr.gov.br>
2025-04-24 07:57:51 -0300

    .

--- projeto 1/main.js
+++ projeto 1/main.js
@@ -2,7 +2,9 @@ const botao = document.querySelectorAll (".botaao");
 
 for (let i =0, < botoes.length, i++){
 
-    botoes[i].onclick = function () 
+    botoes[i].onclick = function () {
     botoes[i].classlist.add("ativo")
+        
+    }
 
 }
